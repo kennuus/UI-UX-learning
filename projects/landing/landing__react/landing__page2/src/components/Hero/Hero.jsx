872d392c28596237ccb5e1c7d836acd6689444cc
kennuu <maribor23072006@gmail.com>
2022-10-18 01:26:03 +0300

    added header

---
+++ projects/landing/landing__react/landing__page2/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='hero'>
+      <div className="hero__left">
+
+      </div>
+      <img src='../assets/Hero'/>
+    </div>
+  )
+}
+
+export default Hero
