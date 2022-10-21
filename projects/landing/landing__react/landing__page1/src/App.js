100354831a0b39398df4a462ee27f19cf359a1a7
kennuu <maribor23072006@gmail.com>
2022-10-21 03:12:15 +0300

    new project

---
+++ projects/landing/landing__react/landing__page1/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import FrstSlide from './components/FrstSlide'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <FrstSlide/>
+    </div>
+  );
+}
+
+export default App;
