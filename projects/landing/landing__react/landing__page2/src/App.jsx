2825ef0c79f0b8a774f8c71a57f2f48deb0cc78f
kennuu <maribor23072006@gmail.com>
2022-10-21 03:11:33 +0300

    added 2 new screens

--- projects/landing/landing__react/landing__page2/src/App.jsx
+++ projects/landing/landing__react/landing__page2/src/App.jsx
@@ -1,11 +1,17 @@
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Services from './components/Services/Services'
+import Portfolio from './components/Portfolio/Portfolio'
+import Experience from './components/Experience/Experience'
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <Header />
       <Hero />
+      <Services />
+      <Portfolio />
+      <Experience />
     </div>
   )
 }
