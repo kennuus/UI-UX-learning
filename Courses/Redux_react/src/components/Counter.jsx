8cd3793312d283d910cb35fe661848f3414b1614
kennuu <maribor23072006@gmail.com>
2022-12-09 23:14:43 +0200

    Added react-redux first mini-project

---
+++ Courses/Redux_react/src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { increment, decrement, incrementByAmount } from '../redux/Counter'
+
+const incValue = (e) => {
+  return e.target.value 
+}
+
+export default function Counter() {
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <div className="flex absolute top-[25px]">
+        <h1> Count: {count}</h1>
+      </div>
+      <button onClick={() => dispatch(increment())}>
+        + 1
+      </button>
+      <button onClick={() => dispatch(decrement())}>
+        - 1
+      </button>
+      <div>
+        <input type="text" onChange={incValue}/>
+        <button onClick={() => dispatch(incrementByAmount({incValue}))}>
+          change on input
+        </button>
+      </div>
+    </div>
+  )
+}
